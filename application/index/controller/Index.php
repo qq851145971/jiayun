d4ced5a908bc0daf9069a2cc9b875406b21209b2
chenbo <chenbo@kdanmobile.com>
2019-05-10 17:34:27 +0800

    jwt引入

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
-
+use think\Db;
+use think\facade\Request;
+use \Firebase\JWT\JWT;
 class Index
 {
     public function index()
@@ -8,8 +10,36 @@ class Index
         return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
     }
 
-    public function hello($name = 'ThinkPHP5')
+    public function hello()
     {
-        return 'hello,' . $name;
+        $info = Request::header();
+        $data=[
+            'id'=>1,
+            'name'=>22
+        ];
+        dump($info);
+      return  json($data)->code(201)->header(['Cache-control' => '1']);
+    }
+    public function jwt(){
+        $key = "huang";  //这里是自定义的一个随机字串，应该写在config文件中的，解密时也会用，相当    于加密中常用的 盐  salt
+        $token = [
+            "iss"=>"",  //签发者 可以为空
+            "aud"=>"", //面象的用户，可以为空
+            "iat" => time(), //签发时间
+            "nbf" => time()+1, //在什么时候jwt开始生效  （这里表示生成100秒后才生效）
+            "exp" => time()+7200, //token 过期时间
+            "uid" => 123 //记录的userid的信息，这里是自已添加上去的，如果有其它信息，可以再添加数组的键值对
+        ];
+        $jwt = JWT::encode($token,$key,"HS512"); //根据参数生成了 token
+        return json([
+            "token"=>$jwt
+        ]);
+    }
+
+    public function check(){
+        $jwt = input("token");  //上一步中返回给用户的token
+        $key = "2f5cdce3b2e1e98d421ab144fa03ad4c0f8d59020a0ec5ec9726a97d277fd23da1909d0475a302818c9bfb98f60dd146da452d9e003ba2746ede8edfbf97288f";  //上一个方法中的 $key 本应该配置在 config文件中的
+        $info = JWT::decode($jwt,$key,["HS512"]); //解密jwt
+        return json($info);
     }
 }
