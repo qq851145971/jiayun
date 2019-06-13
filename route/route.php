ca92f28dcabcd94d5fa4c28ee2da7b0150d8fdb9
chenbo <chenbo@kdanmobile.com>
2019-06-13 15:06:49 +0800

    注册中间件

--- route/route.php
+++ route/route.php
@@ -39,20 +39,20 @@ if (empty($version)){
 /**
  * 路由开始
  */
-Route::get('/', 'index/MyError/kong');
-Route::get('api/files', 'index/'.$v.'.index/files');
-Route::get('api/files/:id', 'index/'.$v.'.index/filesInfo')->pattern(['id' => '\\w{8}(-\\w{4}){3}-\\w{12}?']);
-Route::get('api/members/me', 'index/'.$v.'.index/me');
-Route::post('api/files/batch_delete', 'index/'.$v.'.index/batch_delete');
-Route::post('api/files/move_folder', 'index/'.$v.'.index/moveFolder');
-Route::post('api/files/batch_update', 'index/'.$v.'.index/batch_update');
-Route::post('api/files/share', 'index/'.$v.'.index/share');
-Route::post('api/files/cancel_share', 'index/'.$v.'.index/cancel_share');
-Route::get('api/sharings', 'index/'.$v.'.index/sharings');
-Route::get('s/:name', 'index/'.$v.'.Download/index');
-Route::rule('download/:name', 'index/'.$v.'.Download/download');
-Route::post('oss_qianming', 'index/'.$v.'.index/oss_qianming');
-Route::post('api/files/post_callback', 'index/Aliyun/index');
-Route::post('api/files', 'index/'.$v.'.index/upload');
+Route::get('/', 'index/MyError/kong')->allowCrossDomain();
+Route::get('api/files', 'index/'.$v.'.index/files')->allowCrossDomain();
+Route::get('api/files/:id', 'index/'.$v.'.index/filesInfo')->pattern(['id' => '\\w{8}(-\\w{4}){3}-\\w{12}?'])->allowCrossDomain();
+Route::get('api/members/me', 'index/'.$v.'.index/me')->allowCrossDomain();
+Route::post('api/files/batch_delete', 'index/'.$v.'.index/batch_delete')->allowCrossDomain();
+Route::post('api/files/move_folder', 'index/'.$v.'.index/moveFolder')->allowCrossDomain();
+Route::post('api/files/batch_update', 'index/'.$v.'.index/batch_update')->allowCrossDomain();
+Route::post('api/files/share', 'index/'.$v.'.index/share')->allowCrossDomain();
+Route::post('api/files/cancel_share', 'index/'.$v.'.index/cancel_share')->allowCrossDomain();
+Route::get('api/sharings', 'index/'.$v.'.index/sharings')->allowCrossDomain();
+Route::get('s/:name', 'index/'.$v.'.Download/index')->allowCrossDomain();
+Route::rule('download/:name', 'index/'.$v.'.Download/download')->allowCrossDomain();
+Route::post('oss_qianming', 'index/'.$v.'.index/oss_qianming')->allowCrossDomain();
+Route::post('api/files/post_callback', 'index/Aliyun/index')->allowCrossDomain();
+Route::post('api/files', 'index/'.$v.'.index/upload')->allowCrossDomain();
 return [
 ];
