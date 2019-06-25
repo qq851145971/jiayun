e38b7c0f8c2da58237b3f24a67df34fcf5ec2213
chenbo <chenbo@kdanmobile.com>
2019-06-25 13:21:45 +0800

    数据库增量同步会员类

---
+++ application/index/controller/Synchronization.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 陈大剩
+ * Date: 2019/6/25
+ * Time: 13:18
+ */
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\facade\Log;
+use think\Db;
+class Synchronization extends Controller
+{
+    public function index(){
+        $data=Db::connect(config('env.db2'))->name('members')->limit('10')->select();
+        Log::record($data);
+    }
+}
